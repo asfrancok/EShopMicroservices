febdd1c28660484c32f9a25b24ca2640c9902d66
Aleksandar Sfrancok <ase8764@yahoo.com>
2025-08-01 16:01:13 +0200

    Ordering Domain implemented

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions;
+
+public interface IDomainEvent : INotification
+{
+    Guid EventID => Guid.NewGuid();
+    DateTime OccuredOn => DateTime.UtcNow;
+    string EventType => GetType().AssemblyQualifiedName!;
+}
---
+++ src/Services/Ordering/OrderingAPI/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Carter;
+
+namespace OrderingAPI;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        //services.AddCarter
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        //app.MapCarter
+        return app;
+    }}
